2c9ef9d9d2bcd188caa3736227264cc08a81696f
Nikhil Singh <nikhilrajput4265@gmail.com>
2025-11-11 14:52:22 +0530

    Add validation

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(
 // import the routes
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
-import authRouter from "./routes/auth.routes.js"
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
