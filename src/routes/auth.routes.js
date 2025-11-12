004af8dcdc7159623b2b3ce8adf0e3574177ec1a
Nikhil Singh <nikhilrajput4265@gmail.com>
2025-11-12 13:30:10 +0530

    Add Secure a Logout user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controller.js";
+import {
+  logoutUser,
+  login,
+  registerUser,
+} from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegisterValidator,
   userLoginValidator,
 } from "../validators/index.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
