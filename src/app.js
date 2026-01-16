7ca18250772eb18c48878d827246285dd6791a15
Nikhil Singh <nikhilrajput4265@gmail.com>
2026-01-16 18:42:46 +0530

    Add the router file

--- src/app.js
+++ src/app.js
@@ -26,16 +26,14 @@ app.use(
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import projectRouter from "./routes/project.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/instagram", (req, res) => {
-  res.send("this is an instagram page");
+  res.send("Welcome to World!");
 });
 
 export default app;
